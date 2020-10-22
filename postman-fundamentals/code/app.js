3e7ce6a5976ea753e46ce077d6a98f8d3bb8d06e
yue <yue.liu@authright.com>
2020-10-22 12:04:00 -0400

    mocha

--- postman-fundamentals/code/app.js
+++ postman-fundamentals/code/app.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-if (process.env.ENV === 'Test') {
-  console.log('This is a test');
-  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+if (process.env.ENV === "Test") {
+  console.log("This is a test");
+  const db = mongoose.connect("mongodb://localhost/bookAPI_Test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 } else {
-  console.log('This is for real');
-  const db = mongoose.connect('mongodb://localhost/bookAPI');
+  console.log("This is for real");
+  const db = mongoose.connect("mongodb://localhost/bookAPI", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 }
 
-
 const port = process.env.PORT || 3000;
-const Book = require('./models/bookModel');
-const bookRouter = require('./routes/bookRouter')(Book);
+const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', bookRouter);
+app.use("/api", bookRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Nodemon API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to my Nodemon API!");
 });
 
 app.server = app.listen(port, () => {
---
+++ testing-Mocha/tesingMocha.md
@@ -0,0 +1,9 @@
+# Build a RESTful API with Node.js and Express
+
+## Unit Testing with Mocha
+
+## Building controllers for your routes
+
+## Mock objects with Sinon.js
+
+## Integreation tests with Supertest
